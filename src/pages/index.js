8384afcd803fed50948c7776add6c099b837d2ca
samsvk <samsvk00@gmail.com>
2022-08-23 11:47:30 +0100

    updated display

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Header from "../components/header";
 import Marquee from "../components/marquee";
 import Head from "next/head";
+import Data from "../static";
 
 export default function Home() {
   return (
@@ -12,143 +13,27 @@ export default function Home() {
       <Marquee />
       <Header />
       <div className="max-w-[1800px] w-full mx-auto mb-10">
-        <div className="grid max-h-full min-h-screen grid-cols-6 grid-rows-5 gap-4 py-10">
-          <div className="col-span-1 row-span-5 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"top"}
-                objectFit="cover"
-                src={"/vi/1.jpeg"}
-              />
+        <div className="grid max-h-full min-h-[91vh] grid-cols-6 grid-rows-5 gap-x-3 gap-y-1 py-10">
+          {Data.map((item, index) => (
+            <div
+              key={index}
+              className={`${item.pos.col} ${item.pos.row} overflow-hidden`}
+            >
+              <h1 className="mb-1 text-xs font-light uppercase">
+                0{index + 1} / {item.name}
+              </h1>
+              <div className="relative h-full max-w-full mx-auto my-0">
+                <Image
+                  quality={100}
+                  loading="eager"
+                  priority={true}
+                  layout="fill"
+                  objectFit="cover"
+                  src={`/vi/${item.url}`}
+                />
+              </div>
             </div>
-          </div>
-
-          <div className="col-span-1 row-span-2 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"top"}
-                objectFit="cover"
-                src={"/vi/14.jpg"}
-              />
-            </div>
-          </div>
-
-          <div className="col-span-1 row-span-3 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"top"}
-                objectFit="cover"
-                src={"/vi/2.jpeg"}
-              />
-            </div>
-          </div>
-
-          <div className="col-span-1 row-span-4 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"top"}
-                objectFit="cover"
-                src={"/vi/3.jpeg"}
-              />
-            </div>
-          </div>
-
-          <div className="col-span-1 row-span-3 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"top"}
-                objectFit="cover"
-                src={"/vi/4.jpeg"}
-              />
-            </div>
-          </div>
-          <div className="col-span-1 row-span-3 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"top"}
-                objectFit="cover"
-                src={"/vi/5.jpeg"}
-              />
-            </div>
-          </div>
-          <div className="col-span-1 row-span-3 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"top"}
-                objectFit="cover"
-                src={"/vi/6.jpeg"}
-              />
-            </div>
-          </div>
-
-          <div className="col-span-1 row-span-2 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"top"}
-                objectFit="cover"
-                src={"/vi/8.jpeg"}
-              />
-            </div>
-          </div>
-
-          <div className="col-span-2 row-span-2 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"top"}
-                objectFit="cover"
-                src={"/vi/9.jpeg"}
-              />
-            </div>
-          </div>
-          <div className="col-span-1 row-span-1 overflow-hidden">
-            <div className="relative h-full max-w-full mx-auto my-0">
-              <Image
-                quality={100}
-                loading="eager"
-                priority={true}
-                layout="fill"
-                objectPosition={"middle"}
-                objectFit="cover"
-                src={"/vi/13.jpeg"}
-              />
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
--- src/static.js
+++ src/static.js
@@ -1,6 +1,82 @@
-const data = [
+export default [
   {
     name: "vivixstar",
-    url: "1.jpg",
+    url: "1.jpeg",
+    pos: {
+      col: "col-span-1",
+      row: "row-span-5",
+    },
+  },
+  {
+    name: "sammy",
+    url: "14.jpg",
+    pos: {
+      col: "col-span-1",
+      row: "row-span-2",
+    },
+  },
+  {
+    name: "ironmouse",
+    url: "2.jpeg",
+    pos: {
+      col: "col-span-1",
+      row: "row-span-3",
+    },
+  },
+  {
+    name: "zentreya",
+    url: "3.jpeg",
+    pos: {
+      col: "col-span-1",
+      row: "row-span-4",
+    },
+  },
+  {
+    name: "silvervale",
+    url: "4.jpeg",
+    pos: {
+      col: "col-span-1",
+      row: "row-span-3",
+    },
+  },
+  {
+    name: "vivixstar",
+    url: "5.jpeg",
+    pos: {
+      col: "col-span-1",
+      row: "row-span-3",
+    },
+  },
+  {
+    name: "momotexx",
+    url: "6.jpeg",
+    pos: {
+      col: "col-span-1",
+      row: "row-span-3",
+    },
+  },
+  {
+    name: "xaela",
+    url: "8.jpeg",
+    pos: {
+      col: "col-span-1",
+      row: "row-span-2",
+    },
+  },
+  {
+    name: "commission",
+    url: "9.jpeg",
+    pos: {
+      col: "col-span-2",
+      row: "row-span-2",
+    },
+  },
+  {
+    name: "melparty",
+    url: "13.jpeg",
+    pos: {
+      col: "col-span-1",
+      row: "row-span-1",
+    },
   },
 ];
