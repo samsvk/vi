c375580d2e0fdb4abf209969e42ff2c7765bd2f3
samsvk <samsvk00@gmail.com>
2022-11-20 01:04:44 +0000

    design update

---
+++ src/components/pricing - Copy.js
@@ -0,0 +1,264 @@
+import * as React from "react";
+import { RiCheckLine } from "react-icons/ri";
+
+const POSSIBLE_ART_VARIABLES = {
+  Background: {
+    enabled: true,
+    type: "CHECKBOX",
+    pricing: [{ selected: false, type: "default", price: 50 }],
+  },
+  Basic_Commercial: {
+    enabled: true,
+    type: "CHECKBOX",
+    pricing: [{ selected: false, type: "default", price: 150 }],
+  },
+  Linestyle: {
+    enabled: true,
+    type: "DROPDOWN",
+    pricing: [
+      { selected: true, type: "Line", price: 35 },
+      { selected: false, type: "Flat_Color", price: 45 },
+      { selected: false, type: "Full_Illustration", price: 65 },
+    ],
+  },
+};
+
+const TYPES_OF_CHARACTERS = {
+  Character_Art: {
+    variables: {
+      ...POSSIBLE_ART_VARIABLES,
+    },
+    pricing: [
+      { selected: true, type: "Icon", price: 250 },
+      { selected: false, type: "Half Body", price: 450 },
+      { selected: false, type: "Full Body", price: 650 },
+    ],
+  },
+  "2D_Live_Model_Art": {
+    variables: {
+      ...POSSIBLE_ART_VARIABLES,
+      Linestyle: { enabled: false },
+      Background: { enabled: false },
+    },
+    pricing: [
+      { selected: true, type: "Half Body", price: 1150 },
+      { selected: false, type: "Full Body", price: 1550 },
+    ],
+  },
+};
+
+export default function Pricing() {
+  const CHARACTER_OPTIONS = Object.keys(TYPES_OF_CHARACTERS);
+  const [state, setState] = React.useState({
+    type: CHARACTER_OPTIONS[0],
+    data: TYPES_OF_CHARACTERS[CHARACTER_OPTIONS[0]],
+  });
+  const [price, setPrice] = React.useState(0);
+  const VARIABLE_OPTIONS = Object.keys(state.data.variables);
+
+  React.useEffect(() => {
+    setPrice(
+      state.data.pricing
+        .map((item, index) => (item.selected ? item.price : 0))
+        .concat(
+          VARIABLE_OPTIONS.map((item, index) => {
+            return state.data?.variables[item]?.pricing?.map((_, idx) => {
+              return _.selected ? _.price : 0;
+            });
+          }).flat()
+        )
+        .filter((item) => item !== undefined)
+        .reduce((total, num) => num + total, 0)
+    );
+  }, [state]);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen font-inter min-w-screen bg-main-default text-[#aaa] tracking-tight font-normal text-sm relative">
+      <div className="min-h-[400px] flex items-start justify-center w-full max-w-max ">
+        <div className="relative z-10 flex flex-col justify-center w-full px-6 py-4 border rounded-lg border-white/5 h-max bg-black/10">
+          <div className="py-3 border-b border-white/5">
+            <label className="text-md ">
+              Vi's Price Calculator - Total:{" "}
+              <span className="text-green-700 border-green-700">${price}</span>
+            </label>
+          </div>
+          <div className="py-3 border-b border-white/5">
+            <ul className="flex items-center gap-4">
+              <label className="text-xs opacity-50 ">Type: </label>
+              {CHARACTER_OPTIONS.map((option, index) => {
+                const isSelected = state.type;
+                return (
+                  <li
+                    key={index}
+                    className={`py-1 px-2.5 bg-white/5 rounded-md border border-white/5 hover:cursor-pointer ${
+                      isSelected === option
+                        ? "bg-green-900/10 border-green-700 text-green-700"
+                        : "text-inherit"
+                    }`}
+                    onClick={() =>
+                      setState({ type: option, data: TYPES_OF_CHARACTERS[option] })
+                    }
+                  >
+                    {option.replace(/_/g, " ")}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div className="py-3 border-b border-white/5">
+            <ul className="flex items-center gap-4">
+              <label className="text-xs opacity-50 ">Style:</label>
+              {state.data.pricing.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    className={`py-1 px-2.5 bg-white/5 rounded-md border border-white/5 hover:cursor-pointer ${
+                      item.selected
+                        ? "bg-green-900/10 border-green-700 text-green-700"
+                        : "text-inherit"
+                    }`}
+                    onClick={() =>
+                      setState(() => {
+                        return {
+                          ...state,
+                          data: {
+                            ...state.data,
+                            pricing: state.data.pricing.map((item, idx) => {
+                              return idx === index
+                                ? { ...item, selected: true }
+                                : { ...item, selected: false };
+                            }),
+                          },
+                        };
+                      })
+                    }
+                  >
+                    {item.type}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <ul className="flex flex-col gap-2">
+            {VARIABLE_OPTIONS.map((variable, index) => {
+              const variableOptions = state.data.variables[variable];
+              return (
+                <div key={index}>
+                  <ArtVariableSelection
+                    variable={variable}
+                    variableOptions={variableOptions}
+                    index={index}
+                    state={state}
+                    setState={setState}
+                  />
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const ArtVariableSelection = ({
+  variable,
+  variableOptions,
+  index,
+  state,
+  setState,
+}) => {
+  if (!variableOptions.enabled) return null;
+
+  switch (variableOptions.type) {
+    case "CHECKBOX":
+      return (
+        <div className="py-3 border-b border-white/5">
+          <ul className="flex items-center gap-4">
+            <label className="text-xs opacity-50 ">
+              {variable.replace(/_/g, " ")}:
+            </label>
+            <li
+              onClick={() =>
+                setState(() => {
+                  return {
+                    ...state,
+                    data: {
+                      ...state.data,
+                      variables: {
+                        ...state.data.variables,
+                        [variable]: {
+                          ...state.data.variables[variable],
+                          pricing: state.data.variables[variable].pricing.map(
+                            (item) => ({ ...item, selected: !item.selected })
+                          ),
+                        },
+                      },
+                    },
+                  };
+                })
+              }
+              className={`text-md w-6 h-6 flex items-center justify-center hover:cursor-pointer
+                  transition-[background] bg-white/5 rounded-md border border-white/5 ${
+                    state.data.variables[variable].pricing[0].selected
+                      ? "bg-green-900/10 border-green-700 text-green-700"
+                      : "text-inherit"
+                  }`}
+            >
+              {state.data.variables[variable].pricing[0].selected && (
+                <RiCheckLine style={{ verticalAlign: "middle" }} />
+              )}
+            </li>
+          </ul>
+        </div>
+      );
+    case "DROPDOWN":
+      return (
+        <div className="py-3 border-b border-white/5">
+          <ul className="flex items-center gap-4">
+            <label className="text-xs opacity-50 ">
+              {variable.replace(/_/g, " ")}:
+            </label>
+            {variableOptions.pricing.map((item, index) => (
+              <li
+                className={`py-1 px-2.5 bg-white/5 rounded-md border border-white/5 hover:cursor-pointer  ${
+                  state.data.variables[variable].pricing[index].selected
+                    ? "bg-green-900/10 border-green-700 text-green-700"
+                    : "text-inherit"
+                }`}
+                key={index}
+                onClick={() =>
+                  setState(() => {
+                    return {
+                      ...state,
+                      data: {
+                        ...state.data,
+                        variables: {
+                          ...state.data.variables,
+                          [variable]: {
+                            ...state.data.variables[variable],
+                            pricing: state.data.variables[variable].pricing.map(
+                              (item, idx) => {
+                                return idx === index
+                                  ? { ...item, selected: true }
+                                  : { ...item, selected: false };
+                              }
+                            ),
+                          },
+                        },
+                      },
+                    };
+                  })
+                }
+              >
+                {item.type.replace(/_/g, " ")}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    default:
+      return null;
+  }
+};
--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,21 +1,25 @@
 import * as React from "react";
+import { RiCheckLine } from "react-icons/ri";
 
 const POSSIBLE_ART_VARIABLES = {
   Background: {
+    description: "description",
     enabled: true,
     type: "CHECKBOX",
     pricing: [{ selected: false, type: "default", price: 50 }],
   },
-  Commercial: {
+  Basic_Commercial_Rights: {
+    description: "description",
     enabled: true,
     type: "CHECKBOX",
     pricing: [{ selected: false, type: "default", price: 150 }],
   },
   Linestyle: {
+    description: "description",
     enabled: true,
     type: "DROPDOWN",
     pricing: [
-      { selected: true, type: "Line", price: 30 },
+      { selected: true, type: "Line", price: 35 },
       { selected: false, type: "Flat_Color", price: 45 },
       { selected: false, type: "Full_Illustration", price: 65 },
     ],
@@ -40,20 +44,18 @@ const TYPES_OF_CHARACTERS = {
       Background: { enabled: false },
     },
     pricing: [
-      { selected: true, type: "Icon", price: 650 },
-      { selected: false, type: "Half Body", price: 1150 },
+      { selected: true, type: "Half Body", price: 1150 },
       { selected: false, type: "Full Body", price: 1550 },
     ],
   },
 };
 
-export default function Pricing() {
+export default function Pricing({ setPrice }) {
   const CHARACTER_OPTIONS = Object.keys(TYPES_OF_CHARACTERS);
   const [state, setState] = React.useState({
     type: CHARACTER_OPTIONS[0],
     data: TYPES_OF_CHARACTERS[CHARACTER_OPTIONS[0]],
   });
-  const [price, setPrice] = React.useState(0);
   const VARIABLE_OPTIONS = Object.keys(state.data.variables);
 
   React.useEffect(() => {
@@ -73,72 +75,85 @@ export default function Pricing() {
   }, [state]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen font-inter min-w-screen bg-main-default text-[#aaa] tracking-tight font-normal text-sm relative">
-      <div className="relative z-10 flex flex-col justify-center w-full px-6 py-4 border rounded-lg max-w-max border-white/5 h-max bg-black/10">
-        <div className="py-3 border-b border-white/5">
-          <label className="text-md min-w-[120px]">
-            Vi's Price Calculator - Total:{" "}
-            <span className="text-green-700 border-green-700">${price}</span>
-          </label>
-        </div>
-        <div className="py-3 border-b border-white/5">
-          <ul className="flex items-center gap-4">
-            <label className="text-xs  opacity-50 min-w-[120px]">Type: </label>
-            {CHARACTER_OPTIONS.map((option, index) => {
-              const isSelected = state.type;
-              return (
-                <li
-                  key={index}
-                  className={`py-1 px-2.5 bg-white/5 rounded-md border border-white/5 ${
-                    isSelected === option
-                      ? "bg-green-900/10 border-green-700 text-green-700"
-                      : "text-inherit"
-                  }`}
-                  onClick={() =>
-                    setState({ type: option, data: TYPES_OF_CHARACTERS[option] })
-                  }
-                >
-                  {option.replace(/_/g, " ")}
-                </li>
-              );
-            })}
-          </ul>
+    <div className="w-full">
+      <div className="relative z-10 flex flex-col justify-center gap-5">
+        <div className="flex flex-col">
+          <div className="p-5 rounded-tr-2xl rounded-tl-2xl bg-[#f9f9f9] flex">
+            <div>
+              <h1 className="font-medium leading-loose tracking-tighter text-md">
+                Type of Commission
+              </h1>
+              <p className="text-xs font-medium leading-snug tracking-tight opacity-40">
+                Selected the relevant character type.
+              </p>
+            </div>
+            <ul className="flex items-center gap-4 ml-auto bg-[#f0f0f0] rounded-2xl p-1">
+              {CHARACTER_OPTIONS.map((option, index) => {
+                const isSelected = state.type;
+                return (
+                  <li
+                    key={index}
+                    className={`py-2 px-2.5 bg-white/5 rounded-xl border border-white/5 hover:cursor-pointer font-medium leading-loose tracking-tight text-md ${
+                      isSelected === option ? "bg-white" : "text-inherit"
+                    }`}
+                    onClick={() =>
+                      setState({ type: option, data: TYPES_OF_CHARACTERS[option] })
+                    }
+                  >
+                    {option.replace(/_/g, " ")}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div className="p-5 rounded-br-2xl rounded-bl-2xl bg-[#f9f9f9] flex border-t border-[#f0f0f0] ">
+            <div>
+              <h1 className="font-medium leading-loose tracking-tighter text-md">
+                Style of Commission
+              </h1>
+              <p className="text-xs font-medium leading-snug tracking-tight opacity-40">
+                Selected the relevant character type.
+              </p>
+            </div>
+            <ul className="flex items-center gap-4 ml-auto bg-[#f0f0f0] rounded-2xl p-1">
+              {state.data.pricing.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    className={`py-2 px-2.5 bg-white/5 rounded-xl border border-white/5 hover:cursor-pointer font-medium leading-loose tracking-tight text-md ${
+                      item.selected ? "bg-white" : "text-inherit"
+                    }`}
+                    onClick={() =>
+                      setState(() => {
+                        return {
+                          ...state,
+                          data: {
+                            ...state.data,
+                            pricing: state.data.pricing.map((item, idx) => {
+                              return idx === index
+                                ? { ...item, selected: true }
+                                : { ...item, selected: false };
+                            }),
+                          },
+                        };
+                      })
+                    }
+                  >
+                    {item.type}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
 
-        <div className="py-3 border-b border-white/5">
-          <ul className="flex items-center gap-4">
-            <label className="text-xs  opacity-50 min-w-[120px]">Style</label>
-            {state.data.pricing.map((item, index) => {
-              return (
-                <li
-                  key={index}
-                  className={`py-1 px-2.5 bg-white/5 rounded-md border border-white/5 ${
-                    item.selected
-                      ? "bg-green-900/10 border-green-700 text-green-700"
-                      : "text-inherit"
-                  }`}
-                  onClick={() =>
-                    setState(() => {
-                      return {
-                        ...state,
-                        data: {
-                          ...state.data,
-                          pricing: state.data.pricing.map((item, idx) => {
-                            return idx === index
-                              ? { ...item, selected: true }
-                              : { ...item, selected: false };
-                          }),
-                        },
-                      };
-                    })
-                  }
-                >
-                  {item.type}
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <section>
+          <h1 className="text-[28px] font-medium tracking-tighter leading-loose">
+            Commission Variables
+          </h1>
+        </section>
+
         <ul className="flex flex-col gap-2">
           {VARIABLE_OPTIONS.map((variable, index) => {
             const variableOptions = state.data.variables[variable];
@@ -172,9 +187,17 @@ const ArtVariableSelection = ({
   switch (variableOptions.type) {
     case "CHECKBOX":
       return (
-        <div className="py-3 border-b border-white/5">
+        <div className="p-5 rounded-2xl bg-[#f9f9f9] flex">
           <ul className="flex items-center gap-4">
-            <label className="text-xs opacity-50 min-w-[120px]">{variable}</label>
+            <div>
+              <h1 className="font-medium leading-loose tracking-tighter text-md">
+                {variable.replace(/_/g, " ")}:
+              </h1>
+              <p className="text-xs font-medium leading-snug tracking-tight opacity-40">
+                Selected the relevant character type.
+              </p>
+            </div>
+
             <li
               onClick={() =>
                 setState(() => {
@@ -196,12 +219,16 @@ const ArtVariableSelection = ({
                 })
               }
               className={`text-md w-6 h-6 flex items-center justify-center hover:cursor-pointer
-                  transition-[background] bg-white/5 rounded-md border border-white/5 ${
+                  transition-[background] bg-black rounded-xl border border-white/5 ${
                     state.data.variables[variable].pricing[0].selected
                       ? "bg-green-900/10 border-green-700 text-green-700"
                       : "text-inherit"
                   }`}
-            ></li>
+            >
+              {state.data.variables[variable].pricing[0].selected && (
+                <RiCheckLine style={{ verticalAlign: "middle" }} />
+              )}
+            </li>
           </ul>
         </div>
       );
@@ -209,10 +236,12 @@ const ArtVariableSelection = ({
       return (
         <div className="py-3 border-b border-white/5">
           <ul className="flex items-center gap-4">
-            <label className="text-xs  opacity-50 min-w-[120px]">{variable}</label>
+            <label className="text-xs opacity-50 ">
+              {variable.replace(/_/g, " ")}:
+            </label>
             {variableOptions.pricing.map((item, index) => (
               <li
-                className={`py-1 px-2.5 bg-white/5 rounded-md border border-white/5  ${
+                className={`py-1 px-2.5 bg-white/5 rounded-xl border border-white/5 hover:cursor-pointer  ${
                   state.data.variables[variable].pricing[index].selected
                     ? "bg-green-900/10 border-green-700 text-green-700"
                     : "text-inherit"
